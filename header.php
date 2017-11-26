004a25cc2eaa1d0212fcf29edcb9919f31e53445
Ihor Radchuk <radchuk2808@gmail.com>
2017-11-26 19:24:14 +0200

    Create reposetoty

---
+++ header.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>The Block</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="#">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"/>
+    <link href="style/main.css" rel="stylesheet" type="text/css">
+    <meta name="theme-color" content="#000">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="js/init.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300i,400i,500i" rel="stylesheet">
+</head>
+<body>
+<div id="wrap">
+<header>
+    <nav></nav>
+</header>
