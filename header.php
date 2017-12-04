db668c04d1da5f8839489202bbb7e7d9f0633ae5
Ihor Radchuk <radchuk2808@gmail.com>
2017-12-04 17:17:14 +0200

    update meta tag

--- header.php
+++ header.php
@@ -4,8 +4,7 @@
     <title>The Block</title>
     <meta charset="UTF-8">
     <meta name="description" content="#">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="style/main.css" rel="stylesheet" type="text/css">
     <meta name="theme-color" content="#000">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
